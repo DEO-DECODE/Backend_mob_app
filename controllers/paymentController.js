ded99cf2b431b5c59f58d7e792c90bb849038c6b
DEO RAJ <d.jrajsingh81@gmail.com>
2024-05-24 00:14:00 +0530

    Some changes in payment controller

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,33 +1,32 @@
 import Razorpay from "razorpay";
 import crypto from "crypto";
+
 const razorpay = new Razorpay({
-  key_id: "KEY_ID",
-  key_secret: "KEY_SECRET",
+  key_id: process.env.RAZORPAY_ID_KEY,
+  key_secret: process.env.RAZORPAY_SECRET_KEY,
 });
 
-export const createOrder = async (req, res) => {
+export const createOrder = async (req, res, next) => {
   const { amount, currency, receipt } = req.body;
 
   const options = {
-    amount: amount * 100,
+    amount: amount * 100, 
     currency: currency,
     receipt: receipt,
   };
 
   try {
     const order = await razorpay.orders.create(options);
-    res
-      .status(200)
-      .json({ order, sucess: true, message: "Order Created Successfully" });
+    res.status(200).json({ order, success: true, message: "Order Created Successfully" });
   } catch (error) {
-    next(error);
+    next(error); 
   }
 };
 
-export const verifyPayment = async (req, res) => {
+export const verifyPayment = async (req, res, next) => {
   try {
     const { order_id, payment_id, signature } = req.body;
-    const hmac = crypto.createHmac("sha256", "YOUR_KEY_SECRET");
+    const hmac = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET_KEY); 
     hmac.update(order_id + "|" + payment_id);
     const generatedSignature = hmac.digest("hex");
 
@@ -37,6 +36,6 @@ export const verifyPayment = async (req, res) => {
       res.status(500).json({ status: "failure" });
     }
   } catch (error) {
-    next(error);
+    next(error); 
   }
 };
